9b05406f2758d3c3c6818393b4382ec14c7dd32e
Julian Pollinger <julian.pollinger@jp-studios.de>
2022-06-07 00:16:16 +0200

    build: move rita-core to webpack

--- rita-core/src/index.ts
+++ rita-core/src/index.ts
@@ -3,8 +3,7 @@ export * from './logicElements';
 export * from './Logger';
 export * from './Errors';
 import Parser from './Parser';
-//version number is replaced in build process. Do not change these two lines!
-process.env.VERSION = '0.0.0';
+
 const version = process.env.VERSION;
 
 export { Parser, version };
