0929be39c190c0c6a4bcf8a404a9c6580575d580
Julian Pollinger <julian.pollinger@jp-studios.de>
2022-06-17 09:52:57 +0200

    docs: setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if ! command -v node &> /dev/null
+then
+    echo "node could not be found, please install it"
+    exit
+fi
+
+echo "Installing dependencies..."
+node common/scripts/install-run-rush.js install
+
+echo "Building project"
+node common/scripts/install-run-rush.js build
+
+printf "\n\n\n Project built.\n You can now enter the rita-http directory and start the webserver by running 'node .'\n"
