245cf03e9ad3e2e24ff1037b44fdd3a9f27c45f0
julian <julian@pollinger.dev>
2022-07-05 00:59:51 +0200

    ci: fix [skip ci]

--- .github/exportRitaHttpVersions.sh
+++ .github/exportRitaHttpVersions.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-RITA_HTTP_VERSION=$(cat "$1" \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
+RITA_HTTP_VERSION=$(cat "$1" |
+  grep version |
+  head -1 |
+  awk -F: '{ print $2 }' |
+  sed 's/[",]//g' |
+  sed 's/^ *//g')
 
-IFS='.' read -ra ADDR <<< "$RITA_HTTP_VERSION"
+IFS='.' read -ra ADDR <<<"$RITA_HTTP_VERSION"
 
 RITA_HTTP_VERSION_MAJOR=${ADDR[0]}
 RITA_HTTP_VERSION_MINOR=${ADDR[0]}.${ADDR[1]}
 
-echo "RITA_HTTP_VERSION=$RITA_HTTP_VERSION" >> "$GITHUB_ENV"
-echo "RITA_HTTP_VERSION_MINOR=$RITA_HTTP_VERSION_MINOR" >> "$GITHUB_ENV"
-echo "RITA_HTTP_VERSION_MAJOR=$RITA_HTTP_VERSION_MAJOR" >> "$GITHUB_ENV"
+echo "RITA_HTTP_VERSION=$RITA_HTTP_VERSION" >>"$GITHUB_ENV"
+echo "RITA_HTTP_VERSION_MINOR=$RITA_HTTP_VERSION_MINOR" >>"$GITHUB_ENV"
+echo "RITA_HTTP_VERSION_MAJOR=$RITA_HTTP_VERSION_MAJOR" >>"$GITHUB_ENV"
