04a27497e342eb9817c25c641e20becff245f489
Julian Pollinger <julian@pollinger.dev>
2022-08-27 11:28:07 +0200

    feat(smtlib): add support for string in model

--- rita-smt/node-smtlib/lib/base_solver.ts
+++ rita-smt/node-smtlib/lib/base_solver.ts
@@ -6,69 +6,66 @@
 
 import * as smt from './smtlib';
 
+export type Model = Record<string, number | boolean | string>;
+
 export type SatResult = {
-    satisfieable : boolean,
-    model : Record<string,number|boolean>|undefined
-}
+    satisfieable: boolean;
+    model: Model | undefined;
+};
 
 export default class BaseSmtSolver {
-    private _statements : smt.SNode[];
-    withAssignments : boolean;
-    timeLimit : number;
-    placeholders : string[] = [];
-    public readonly output : string[] = [];
+    private _statements: smt.SNode[];
+    withAssignments: boolean;
+    timeLimit: number;
+    placeholders: string[] = [];
+    public readonly output: string[] = [];
 
     constructor(logic = 'QF_ALL_SUPPORTED') {
-        this._statements = [
-            smt.SetLogic(logic)
-        ];
-
+        this._statements = [smt.SetLogic(logic)];
 
         this.withAssignments = false;
         this.timeLimit = 180000;
     }
 
-    addPlaceholder(s : string) {
+    addPlaceholder(s: string) {
         this.placeholders.push(s);
     }
 
-    isPlaceholder(s : string) : boolean {
+    isPlaceholder(s: string): boolean {
         return this.placeholders.includes(s);
     }
 
-    enableAssignments() : void {
+    enableAssignments(): void {
         this.withAssignments = true;
         this.add(smt.SetOption('produce-assignments'));
         this.add(smt.SetOption('produce-models'));
     }
 
-    dump(f : ((s : string) => void) = console.log) : void {
-        for (const stmt of this._statements)
-            f(stmt.toString());
-
+    dump(f: (s: string) => void = console.log): void {
+        for (const stmt of this._statements) f(stmt.toString());
     }
 
-    forEachStatement(callback : (cb : smt.SNode, idx : number) => void) : void {
+    forEachStatement(callback: (cb: smt.SNode, idx: number) => void): void {
         this._statements.forEach(callback);
     }
 
-    checkSat() : Promise<SatResult> {
+    checkSat(): Promise<SatResult> {
         throw new Error('checkSat not implemented for this solver');
     }
 
-    add(stmt : smt.SNode) : void {
+    add(stmt: smt.SNode): void {
         this._statements.push(stmt);
     }
 
-    assert(expr : smt.SNode) : void {
+    assert(expr: smt.SNode): void {
         this.add(smt.Assert(expr));
     }
 
-    setOption(opt : string, value : smt.SNode = 'true') : void {
+    setOption(opt: string, value: smt.SNode = 'true'): void {
         this.add(smt.SetOption(opt, value));
     }
 
-    declareFun(name : string, args : smt.SNode[], type : smt.SNode) : void {
+    declareFun(name: string, args: smt.SNode[], type: smt.SNode): void {
         this.add(smt.DeclareFun(name, args, type));
     }
 }
