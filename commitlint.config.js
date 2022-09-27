db2f4deb0e3ca717d50e9913ef125b052d12d581
julian <julian@pollinger.dev>
2022-09-27 15:43:15 +0200

    chore: edit commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,18 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        "footer-max-line-length": [1, "always", 100]
-    }
-}
+        'footer-max-line-length': [1, 'always', 100],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'core',
+                'persistent-rita',
+                'smtlib',
+                'rita-smt',
+                'rita-plugin-http',
+                'rita-http',
+            ],
+        ],
+    },
+};
