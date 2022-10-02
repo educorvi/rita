7ec3383c961fa203e059cb63f30c2ad2d78b2308
julian <julian@pollinger.dev>
2022-10-02 18:20:04 +0200

    chore: commitlint settings

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'footer-max-line-length': [1, 'always', 100],
+        'header-max-length': [1, 'always', 100],
         'scope-enum': [
             2,
             'always',
