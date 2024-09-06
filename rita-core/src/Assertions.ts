240b6f363649e04b180d0d1e757611c8b9994459
Julian Pollinger <julian@pollinger.dev>
2024-09-06 15:59:25 +0200

    fix: don't fail on error due to calling `toString()` on undefined

--- rita-core/src/Assertions.ts
+++ rita-core/src/Assertions.ts
@@ -1,43 +1,45 @@
 import { Duration } from 'luxon';
 
-export function assertBoolean(value: any): asserts value is boolean {
+export function assertBoolean(value: unknown): asserts value is boolean {
     if (typeof value !== 'boolean') {
-        throw new TypeError('Value is not a boolean: ' + value.toString());
+        throw new TypeError('Value is not a boolean: ' + value?.toString());
     }
 }
 
-export function assertNumber(value: any): asserts value is number {
+export function assertNumber(value: unknown): asserts value is number {
     if (typeof value !== 'number') {
-        throw new TypeError('Value is not a number: ' + value.toString());
+        throw new TypeError('Value is not a number: ' + value?.toString());
     }
 }
 
-export function assertDate(value: any): asserts value is Date {
+export function assertDate(value: unknown): asserts value is Date {
     if (!(value instanceof Date)) {
-        throw new TypeError('Value is not a date: ' + value.toString());
+        throw new TypeError('Value is not a date: ' + value?.toString());
     }
 }
 
-export function assertNumberOrDate(value: any): asserts value is number | Date {
+export function assertNumberOrDate(
+    value: unknown
+): asserts value is number | Date {
     if (!(typeof value === 'number' || value instanceof Date)) {
         throw new TypeError(
-            'Value is neither a number nor a data: ' + value.toString()
+            'Value is neither a number nor a data: ' + value?.toString()
         );
     }
 }
 
-export function assertDuration(value: any): asserts value is Duration {
+export function assertDuration(value: unknown): asserts value is Duration {
     if (!Duration.isDuration(value)) {
-        throw new TypeError('Value is not a duration: ' + value.toString());
+        throw new TypeError('Value is not a duration: ' + value?.toString());
     }
 }
 
 export function hasLength(
-    value: any
+    value: unknown
 ): asserts value is Record<any, any> & { length: number } {
     if (!value || !value.hasOwnProperty('length')) {
         throw new TypeError(
-            'Value does not have a length property: ' + value.toString()
+            'Value does not have a length property: ' + value?.toString()
         );
     }
 }
